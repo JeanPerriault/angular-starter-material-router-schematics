31d969cd3fd9f931f7976a5260e6e133de7fa7ab
Jean Perriault <jpweb.taf@gmail.com>
2018-09-12 23:13:44 +0200

    Initial navigation component

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  title = "Angular Starter - Material, router and schematics";
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
+    
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+
+  }  
+}
